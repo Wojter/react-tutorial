8a68fb4e6ffeb8bb0a46bdbb20beabe64618e7b9
Wojter <wojtektro2@gmail.com>
2022-05-12 21:22:54 +0200

    Click event and useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 const Home = () => {
+    //let name = 'mario';
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(25);
 
-    return (
+    const handleClick = (e) => {
+        console.log("hello, everyone", e);
+    }
+
+const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+}
+const handleClick2 = () => {
+    //name = "luigi";
+    setName("luigi");
+    setAge(30);
+}
+  return (
     <div className="home">
-
+        <h2>Homepage</h2>
+        <p>{ name } is {age} years old</p>
+        <button onClick={handleClick2}>Change name</button>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) =>
+            handleClickAgain('mario', e)
+            }>Click me again</button>
     </div>
   )
 }
