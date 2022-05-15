33461aa3830bfff8c9dda42cfe9e4cf832d3ad9f
Wojter <wojtektro2@gmail.com>
2022-05-15 16:30:37 +0200

    Added delete blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,7 +3,16 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+
+    const handleClick= () => {
+        fetch('http://localhost:8000/blogs/'+blog.id, {
+            method: 'DELETE',
+        }).then(()=>{
+            console.log('deltete');
+        })
+    }
+
     return ( 
         <div className="blog-details">
             <h2>Blog Details - { id }</h2>
@@ -14,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
